0d4a6ea1fba6268fd1bd06eb6165b878a049448b
gigaherz <gigaherz@gmail.com>
2016-06-28 18:50:33 +0200

    Move the test mod to its own sourceSet.

---
+++ src/main/java/gigaherz/graph/api/Constants.java
@@ -0,0 +1,6 @@
+package gigaherz.graph.api;
+
+public class Constants
+{
+    public static final String API_VERSION = "1.0.0";
+}
---
+++ src/main/java/gigaherz/graph/api/package-info.java
@@ -0,0 +1,5 @@
+
+@API(owner = "GraphLib", provides="GraphApi", apiVersion = Constants.API_VERSION)
+package gigaherz.graph.api;
+
+import net.minecraftforge.fml.common.API;
