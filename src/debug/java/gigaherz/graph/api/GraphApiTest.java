382d9ab7323e8520c11cc67fbc74a8601303170c
gigaherz <gigaherz@gmail.com>
2016-06-29 02:26:44 +0200

    for some stupid reason, IDEA is a bit too aggressive with removing things it deems unnecessary. Does not affect the release.

--- src/debug/java/gigaherz/graph/api/GraphApiTest.java
+++ src/debug/java/gigaherz/graph/api/GraphApiTest.java
@@ -14,7 +14,7 @@ public class GraphApiTest
     public static final String MODID = "GraphLib";
     public static final String VERSION = "1.0";
 
-    @SidedProxy(clientSide = "ClientProxy", serverSide = "Proxy")
+    @SidedProxy(clientSide = "gigaherz.graph.api.ClientProxy", serverSide = "gigaherz.graph.api.Proxy")
     public static Proxy proxy;
 
     public static BlockRegistered networkTest;
@@ -35,6 +35,6 @@ public class GraphApiTest
     {
         proxy.init();
 
-        FMLInterModComms.sendMessage("Waila", "register", "WailaProviders.callbackRegister");
+        FMLInterModComms.sendMessage("Waila", "register", "gigaherz.graph.api.WailaProviders.callbackRegister");
     }
 }
