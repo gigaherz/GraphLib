03c054966c9f665de591ac33ff59f775e290a61c
gigaherz <gigaherz@gmail.com>
2018-09-14 09:59:28 +0200

    Update to latest forge and mappings.

--- src/debug/java/gigaherz/graph2/BlockRegistered.java
+++ src/debug/java/gigaherz/graph2/BlockRegistered.java
@@ -1,24 +1,17 @@
 package gigaherz.graph2;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.ItemBlock;
 
 public class BlockRegistered extends Block
 {
-    public BlockRegistered(String name, Material blockMaterialIn, MapColor blockMapColorIn)
-    {
-        super(blockMaterialIn, blockMapColorIn);
-        setRegistryName(name);
-        setUnlocalizedName(GraphApiTest.MODID + "." + name);
-    }
 
     public BlockRegistered(String name, Material materialIn)
     {
         super(materialIn);
         setRegistryName(name);
-        setUnlocalizedName(GraphApiTest.MODID + "." + name);
+        setTranslationKey(GraphApiTest.MODID + "." + name);
     }
 
     public ItemBlock createItemBlock()
