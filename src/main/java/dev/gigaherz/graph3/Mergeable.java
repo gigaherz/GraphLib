9fc8bb54ef9503b87e986060bbc9090b43289dd2
gigaherz <gigaherz@gmail.com>
2021-10-01 23:03:33 +0200

    Add dummy mergeable.

--- src/main/java/dev/gigaherz/graph3/Mergeable.java
+++ src/main/java/dev/gigaherz/graph3/Mergeable.java
@@ -4,4 +4,24 @@ public interface Mergeable<T extends Mergeable<T>>
 {
     T mergeWith(T other);
     T copy();
+
+    @PublicApi
+    Dummy DUMMY = new Dummy();
+
+    class Dummy implements Mergeable<Dummy>
+    {
+        private Dummy(){}
+
+        @Override
+        public Dummy mergeWith(Dummy other)
+        {
+            return this;
+        }
+
+        @Override
+        public Dummy copy()
+        {
+            return this;
+        }
+    }
 }
